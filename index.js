5e9135ee06421559d4e8f5ba065c41b6b6098397
nnguyen1206 <70247531+nnguyen1206@users.noreply.github.com>
2022-10-31 12:00:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,40 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+// import {AppRegistry} from 'react-native';
+// import App from './App';
+// import {name as appName} from './app.json';
+//
+// AppRegistry.registerComponent(appName, () => App);
 
-AppRegistry.registerComponent(appName, () => App);
+
+
+import React, {Component} from 'react';
+import {AppRegistry, AppState} from 'react-native';
+import {Provider} from 'react-redux';
+import store from './stores';
+import AppNavigator from'./AppNavigator';
+import Location from "./screens/authorized/Location/LocationScreen";
+class Application extends Component {
+
+    // constructor(props) {
+    //     super(props);
+    //     this.appState = AppState.currentState;
+    //     AppState.addEventListener('change', (nextAppState) => {
+    //         console.log('appstate', nextAppState);
+    //         if (this.appState.match(/active/) && (nextAppState === 'background' || nextAppState === 'inactive')) {
+    //             AppRegistry.registerHeadlessTask('SomeTaskName', () => console.log('helodsfdskj'));
+    //         }
+    //     });
+    // }
+
+    render() {
+        return (
+            <Provider store={store}>
+                <AppNavigator/>
+            </Provider>
+        );
+    }
+}
+
+AppRegistry.registerComponent('LoveApp', () => Application);
